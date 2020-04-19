673ae08eb6c6d47ceefc7893f2497ab1b39cd844
alex <slipshot21@gmail.com>
2020-04-19 16:19:37 -0500

    basic front-end set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
+//React and ReactRouter imports
+import React, {Fragment, useEffect} from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+
+//Redux Imports
+import {Provider} from 'react-redux'
+import store from "./store"
+
+//Component Imports
+import NavBar from './components/layout/NavBar'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store} >
+      <Router>
+        <Fragment>
+          <NavBar />
+          <h1>Hello</h1>
+          <Switch>
+            
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ client/src/components/layout/NavBar.js
@@ -0,0 +1,21 @@
+import React, {Fragment, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+
+const NavBar = props => {
+    //@TODO add useeffect to load username to navbar
+    return (
+        <Fragment>
+            <div className = 'container'>
+
+            </div>
+        </Fragment>
+    )
+}
+
+NavBar.propTypes = {
+
+}
+
+export default connect(null, {})(NavBar)
