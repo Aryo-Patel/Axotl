803fd779c1b34c11fcbde112eac485e7830ec4c0
alex <slipshot21@gmail.com>
2020-04-19 16:59:46 -0500

    some navbar adds

--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -1,21 +1,59 @@
-import React, {Fragment, useEffect} from 'react'
-import {Link} from 'react-router-dom'
-import {connect} from 'react-redux'
-import PropTypes from 'prop-types'
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-const NavBar = props => {
-    //@TODO add useeffect to load username to navbar
-    return (
-        <Fragment>
-            <div className = 'container'>
-
+const NavBar = (props) => {
+  //@TODO add useeffect to load username to navbar
+  const authRecLinks = (
+    <ul>
+      <li>
+        <Link to="/recipientProfiles">Hackathons</Link>
+      </li>
+      <li>
+        <Link to="/posts">Posts</Link>
+      </li>
+      <li>
+        <Link to="/chat">Chat</Link>
+      </li>
+    </ul>
+  );
+  const authSponsLinks = (
+    <ul>
+    <li>
+      <Link to="/sponsorProfiles">Sponsor Organizations</Link>
+    </li>
+    <li>
+      <Link to="/posts">Posts</Link>
+    </li>
+    <li>
+      <Link to="/chat">Chat</Link>
+    </li>
+  </ul>
+  )
+  //@TODO set navbar links based on authentication and sponsor status
+  return (
+    <Fragment>
+      <div
+        className="container-fluid "
+        style={{ backgroundColor: "#87ffa7", opacity: "75%" }}
+      >
+        <div className="row">
+          <div className="col col-8">
+            <div className="row">
+              <div className="col col-4 float-left align-content-left">
+                <h3>Axotl</h3>
+              </div>
+              <div className="col col-8"></div>
             </div>
-        </Fragment>
-    )
-}
+          </div>
+          <div className="col col-4">{authRecLinks}</div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
-NavBar.propTypes = {
+NavBar.propTypes = {};
 
-}
-
-export default connect(null, {})(NavBar)
+export default connect(null, {})(NavBar);
