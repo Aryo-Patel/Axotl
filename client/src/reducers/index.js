603b6911f4c4c6591fdb5a60a9ebd2be9b4d293d
Nadharm <nadharmdhiantravan@gmail.com>
2020-04-24 16:56:23 -0500

    fixed typo in last commit. oops

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
-import profileReducer from './profileRecucer';
+import profileReducer from './profileReducer';
+
 export default combineReducers({
     profile: profileReducer,
 })
