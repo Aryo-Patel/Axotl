58d59b36e82dbb04422d2e63c1461e517f6d9dfd
Nadharm <nadharmdhiantravan@gmail.com>
2020-05-10 21:15:08 -0500

    Updates to the SponsorProfile

--- models/SponsorProfile.js
+++ models/SponsorProfile.js
@@ -59,6 +59,9 @@ let SponsorProfileSchema = new Schema({
         instagram: {
             type: String
         },
+        youtube: {
+            type: String
+        },
     },
     distanceFromUser: {
         type: String
