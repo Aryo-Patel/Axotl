cb4cacad5957bb1fcd49717521f49b69e448dbc9
alex <slipshot21@gmail.com>
2020-05-23 18:00:17 -0500

    post redux work and front end work, user model edits

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
+import {createPost, editPost, getPosts} from '../../actions/post';
+import {connect} from 'react-redux'
+import {Redirect, Link, withRouter} from 'react-router-dom';
 
-const Posts = props => {
+const Posts = ({}) => {
+    useEffect(() => {
+        getPosts();
+    }, [getPosts])
+    const [search, setSearch] = useState('')
     return (
         <div className = 'posts'>
-            
+            <h3 className="heading">Posts</h3>
+            <form className="searchingContainer" onSubmit = {e=> onSubmit(e)}>
+                <input type="text" className="searchBar" placeholder='Search for a sponsor...' onChange = {e => onChange(e)} value= {search}/>
+                <input type='submit' className = 'search' value='Search'/>
+            </form>
         </div>
     )
 }
@@ -13,4 +24,4 @@ Posts.propTypes = {
 
 }
 
-export default Posts
+export default connect(null, {})(Posts)
