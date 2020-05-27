c12ff11466932d32aa396d6250e1d24094ddb491
AryoP <aryopatel@gmail.com>
2020-05-27 08:37:00 -0400

    added the styles to the view my hackathons side

--- client/src/components/hackathons/MyHackathons.js
+++ client/src/components/hackathons/MyHackathons.js
@@ -5,6 +5,9 @@ import { getUserHackathons } from '../../actions/hackathonActions';
 
 import RecipientHackathon from './RecipientHackathon';
 
+
+//jquery import
+import $ from 'jquery';
 //import the styling
 import '../discover/styling/hackathons.css';
 
@@ -16,9 +19,49 @@ const MyHackathons = ({ getUserHackathons, id, hackathons, loading }) => {
         getUserHackathons(id)
     }, [getUserHackathons, id])
 
-    console.log('test');
+    //a bit of jquery for animating the hackathons so they fade in
+    $(document).ready(function () {
+        let centerX = $(window).width() / 2;
+
+        $(".hackathon-item").each(function () {
+            $(this).position().left > centerX ? $(this).css({ 'transform': 'translate(300px, 0px)' }) : $(this).css({ 'transform': 'translate(-300px, 0px)' });
+
+            $(this).hover(() => {
+                $(this).css({ 'transform': 'scale(1.05)' });
+            },
+                () => {
+                    $(this).css({ 'transform': 'scale(1)' });
+                })
+            if ($(this).position().top + $(this).height() - $(this).height() * 0.5 < $(window).scrollTop() + $(window).height()) {
+                $(this).css({ 'transition': '1s', 'transform': 'translate(0px, 0px)', 'opacity': '1' });
+            }
+        })
+        $(window).resize(function () {
+            centerX = $(window).width() / 2;
+        });
+        $(window).scroll(function (i) {
+            $(".hackathon-item").each(function (i) {
+                let objectBottom = $(this).position().top + $(this).height() - $(this).height() * 0.5;
+                let windowBottom = $(window).scrollTop() + $(window).height();
+                let windowTop = $(window).scrollTop();
+                if (objectBottom < windowBottom) {
+                    $(this).css({ 'transition': '1s', 'transform': 'translate(0px, 0px)', 'opacity': '1' });
+                }
+                if (objectBottom > windowBottom || objectBottom < windowTop - 200) {
+                    $(this).position().left > centerX ? $(this).css({ 'transform': 'translate(300px, 0px)', 'opacity': '0' }) : $(this).css({ 'transform': 'translate(-300px, 0px)', 'opacity': '0' });
+                }
+                // if (objectBottom < windowTop) {
+                //     console.log('this ran');
+                //     $(this).position().left > centerX ? $(this).css({ 'transform': 'translate(300px, 0px)', 'opacity': '0' }) : $(this).css({ 'transform': 'translate(-300px, 0px)', 'opacity': '0' });
+                // }
+            })
+        })
+    })
     return (
         <Fragment>
+            <div className='top-banner'>
+                <h1>Your Hackathons</h1>
+            </div>
             <div className="hackathon-container">
                 {!loading ?
                     hackathons.map((hackathon, index) => (
--- client/src/components/hackathons/RecipientHackathon.js
+++ client/src/components/hackathons/RecipientHackathon.js
@@ -11,34 +11,38 @@ const RecipientHackathon = ({ hackathon }) => {
         return <Redirect to={`/hackathon/${hackathon._id}`} />
     }
     return (
-        <div className="hackathon-item" onClick={e => onClick(e)}>
-            <div className="hackathon-content">
-                <h1>Title: {hackathon.title}</h1>
+        <div className='hackathon-holder'>
 
-                <p className="description">Description: {hackathon.description}</p>
-                {hackathon.website ? <p className="website">Website: {hackathon.website}</p> : <p>Organizer didn't link their website!</p>}
-                <p>Donations requested: </p>
-                {hackathon.donations.map(donation => (
-                    donation.received.length > 0 ?
-                        <p></p>
-                        :
-                        <p>{donation.type}</p>
-                    // donation.received.length > 0 ?
-                    //     donation.received.map(received_singular => (
-                    //         <p key={i++}>Received {received_singular.type} : {received_singular.description}</p>
-                    //     ))
-                    //     :
-                    //     <Fragment></Fragment>
-                ))}
-                {hackathon.location ? <p>Location : {hackathon.location}</p> : <p>Organizer did not link hackathon's location</p>}
-                <p>Start Date: <Moment format='MM/DD/YYYY'>{hackathon.startDate}</Moment></p>
 
-                <p>End Date: <Moment format='MM/DD/YYYY'>{hackathon.endDate}</Moment></p>
-                <Link to={`/hackathon/${hackathon._id}`} className="btn btn-primary">
-                    View Hackathon
+            <div className="hackathon-item" onClick={e => onClick(e)}>
+                <div className="hackathon-content">
+                    <h1>Title: {hackathon.title}</h1>
+
+                    <p className="description">Description: {hackathon.description}</p>
+                    {hackathon.website ? <p className="website">Website: {hackathon.website}</p> : <p>Organizer didn't link their website!</p>}
+                    <p>Donations requested: </p>
+                    {hackathon.donations.map(donation => (
+                        donation.received.length > 0 ?
+                            <p></p>
+                            :
+                            <p>{donation.type}</p>
+                        // donation.received.length > 0 ?
+                        //     donation.received.map(received_singular => (
+                        //         <p key={i++}>Received {received_singular.type} : {received_singular.description}</p>
+                        //     ))
+                        //     :
+                        //     <Fragment></Fragment>
+                    ))}
+                    {hackathon.location ? <p>Location : {hackathon.location}</p> : <p>Organizer did not link hackathon's location</p>}
+                    <p>Start Date: <Moment format='MM/DD/YYYY'>{hackathon.startDate}</Moment></p>
+
+                    <p>End Date: <Moment format='MM/DD/YYYY'>{hackathon.endDate}</Moment></p>
+                    <Link to={`/hackathon/${hackathon._id}`} className="btn btn-primary">
+                        View Hackathon
                 </Link>
-            </div>
+                </div>
 
+            </div>
         </div>
     )
 }
